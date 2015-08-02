4a28a66f2b39f041768cbfad55bfcfce36ffdf68
MishaHome <misha@example.com>
2015-08-02 22:54:46 +0300

    базовый шаблон

---
+++ src/AppBundle/Resources/public/js/vendor.initialization.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.your-class').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover:false,
+        arrows: false,
+        slidesToShow: 2,
+        slidesToScroll: 2
+    });
+});
