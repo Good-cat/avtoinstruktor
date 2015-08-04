a59ccea7f00dfa05c436d9967679801e0e48dc82
Misha <m@m.m>
2015-08-04 18:38:14 +0300

    entity contact

---
+++ src/AppBundle/Entity/Contact.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="contact")
+ */
+
+class Contact {
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $phones;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $email;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $skype;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $address;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $information;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set phones
+     *
+     * @param string $phones
+     * @return Contact
+     */
+    public function setPhones($phones)
+    {
+        $this->phones = $phones;
+
+        return $this;
+    }
+
+    /**
+     * Get phones
+     *
+     * @return string 
+     */
+    public function getPhones()
+    {
+        return $this->phones;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Contact
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set skype
+     *
+     * @param string $skype
+     * @return Contact
+     */
+    public function setSkype($skype)
+    {
+        $this->skype = $skype;
+
+        return $this;
+    }
+
+    /**
+     * Get skype
+     *
+     * @return string 
+     */
+    public function getSkype()
+    {
+        return $this->skype;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     * @return Contact
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string 
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set information
+     *
+     * @param string $information
+     * @return Contact
+     */
+    public function setInformation($information)
+    {
+        $this->information = $information;
+
+        return $this;
+    }
+
+    /**
+     * Get information
+     *
+     * @return string 
+     */
+    public function getInformation()
+    {
+        return $this->information;
+    }
+}
--- src/CallMeBundle/Controller/CallMeController.php
+++ src/CallMeBundle/Controller/CallMeController.php
@@ -4,13 +4,38 @@ namespace CallMeBundle\Controller;
 
 use CallMeBundle\Form\Type\CallMeType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CallMeController extends Controller{
+
     public function getCallMeFormAction()
     {
         $callMeForm = $this->createForm(new CallMeType());
         return $this->render('CallMeBundle::_callme.html.twig', array(
             'callMeForm' => $callMeForm->createView()
         ));
+    }
+
+    public function callMeAction(Request $request)
+    {
+        if ($request->isXmlHttpRequest()) {
+            $callMeForm = $this->createForm(new CallMeType());
+            $callMeForm->handleRequest($request);
+            if ($callMeForm->isValid()){
+                $name = $callMeForm->get('name')->getData();
+                $phone = $callMeForm->get('phone')->getData();
+                $message = $callMeForm->get('message')->getData() ? : "Сообщение отсутствует";
+                $callme = $this->get($this->container->getParameter('call_me_service'));
+                if ($callme->send($name, $phone, $message)) {
+                    return new Response('Данные успешно отправлены, наш менеджер перезвонит Вам в ближайшее время.');
+                } else {
+                    return new Response('Ошибка при отправке данных, проверьте соединение с Интернетом или повторите попытку позже.');
+                }
+            } else {
+                return new Response('Поля Имя и Телефон являются обязательными для заполнения!');
+            }
+
+        }
     }
 } 
