14ce61a8c9c9e1588a00a28f13184b6e9fb5d87e
Misha <m@m.m>
2015-08-15 17:30:25 +0300

    service entity

---
+++ src/AppBundle/Entity/Service.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use AppBundle\Utils\Slugify;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="service")
+ */
+class Service {
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $annotation;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $visible;
+
+//    private $articles;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $update_at;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $slug;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Service
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set annotation
+     *
+     * @param string $annotation
+     * @return Service
+     */
+    public function setAnnotation($annotation)
+    {
+        $this->annotation = $annotation;
+
+        return $this;
+    }
+
+    /**
+     * Get annotation
+     *
+     * @return string 
+     */
+    public function getAnnotation()
+    {
+        return $this->annotation;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Service
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set visible
+     *
+     * @param boolean $visible
+     * @return Service
+     */
+    public function setVisible($visible)
+    {
+        $this->visible = $visible;
+
+        return $this;
+    }
+
+    /**
+     * Get visible
+     *
+     * @return boolean 
+     */
+    public function getVisible()
+    {
+        return $this->visible;
+    }
+
+    /**
+     * Set update_at
+     *
+     * @param \DateTime $updateAt
+     * @return Service
+     */
+    public function setUpdateAt($updateAt)
+    {
+        $this->update_at = $updateAt;
+
+        return $this;
+    }
+
+    /**
+     * Get update_at
+     *
+     * @return \DateTime 
+     */
+    public function getUpdateAt()
+    {
+        return $this->update_at;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     * @return Service
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = Slugify::slug($this->name);
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+}
